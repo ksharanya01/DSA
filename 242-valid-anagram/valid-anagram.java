5e643e030225ea9a6df35f9a35dc514a7a095c9d
Kothoori Sharanya <23r01a6725@cmrithyderabad.edu.in>
2026-05-02 08:08:26 +0530

    Time: 25 ms (5.39%) | Memory: 44.7 MB (56.36%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int arr[]=new int[26];
+        HashMap<Character,Integer>map=new HashMap<>();
         if(s.length()!=t.length()){
             return false;
         }
-        for(int i=0;i<s.length();i++){
-            arr[s.charAt(i)-'a']++;
-            arr[t.charAt(i)-'a']--;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+            map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)-1);
         }
-        for(int i=0;i<26;i++){
-            if(arr[i]>0){
+        for(char ch:map.keySet()){
+            if(map.get(ch)>0){
                 return false;
             }
         }
         return true;
-        
     }
+        
 }
