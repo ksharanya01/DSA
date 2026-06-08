61bf4a3e46f61c63778965dbb44a5260af9d7c82
Kothoori Sharanya <23r01a6725@cmrithyderabad.edu.in>
2026-06-08 16:47:34 +0530

    Time: 4 ms (99.24%) | Memory: 160.1 MB (26.79%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,30 +1,19 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int x=0;
-        int piv[]=new int[nums.length];
-        ArrayList<Integer>list=new ArrayList<>();
-        ArrayList<Integer>equal=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]<pivot){
-                piv[x]=nums[i];
-                x++;
+        int i1 = 0;
+        int j1 = nums.length - 1;
+        int[] ans = new int[nums.length];
 
-            }else if(nums[i]>pivot){
-                list.add(nums[i]);
-                
-            }else{
-                equal.add(pivot);
-            }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < pivot) ans[i1++] = nums[i];
         }
-        for(int z:equal){
-            piv[x]=z;
-            x++;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] > pivot) ans[j1--] = nums[i];
         }
-        for(int z:list){
-            piv[x]=z;
-            x++;
+        for (int k = i1; k <= j1; k++) {
+            ans[k] = pivot;
         }
-        return piv;
-        
+
+        return ans;
     }
 }
